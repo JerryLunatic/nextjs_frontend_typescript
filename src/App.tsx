fd26bc6f1ce17ac679dbb42b3b7a2a848e511c33
JerryLunatic <55341853+JerryLunatic@users.noreply.github.com>
2024-03-17 16:02:39 +0530

    reverted changes App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch('http://172.18.0.2:8080/data');
+            const response = await fetch('http://localhost:8080/data');
             const data = await response.json();
             setPeople(data);
             console.log(data)
